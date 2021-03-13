a9f218c899a55e936e598163c304d01224a37003
hoapha1009 <hoapha1009@gmail.com>
2021-03-13 16:08:13 +0700

    Feat: SearchInput component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,30 @@
 import { GetServerSideProps } from "next";
+import { useState } from "react";
 import CountriesTable from "../components/CountriesTable/CountriesTable";
 import Global from "../components/Global/Global";
 import Layout from "../components/Layout/Layout";
+import SearchInput from "../components/SearchInput/SearchInput";
 import styles from "../styles/Home.module.css";
 
 export default function Home({ global, countries }) {
+    const [keyword, setKeyword] = useState<string>("");
+
+    const filteredCountries = countries.filter((c) => {
+        return (
+            c.Country.toLowerCase().includes(keyword) ||
+            c.CountryCode.toLowerCase().includes(keyword) ||
+            c.Slug.toLowerCase().includes(keyword)
+        );
+    });
+
+    const onHandleChangeSearch = (
+        e: React.FormEvent<HTMLInputElement>
+    ): void => {
+        e.preventDefault();
+
+        setKeyword(e.currentTarget.value.toLowerCase());
+    };
+
     return (
         <Layout title="Covid19 Tracker">
             <div className={styles.counts}>
@@ -12,7 +32,12 @@ export default function Home({ global, countries }) {
                 Found {countries.length} countries
             </div>
 
-            <CountriesTable countries={countries} />
+            <SearchInput
+                onChange={onHandleChangeSearch}
+                placeholder="Filter by Country name, Country code, Slug..."
+            />
+
+            <CountriesTable countries={filteredCountries} />
         </Layout>
     );
 }
